de86a54a5a8e079c5b02486bec8d51fcc1caaa7b
shanushka <anushkashrestha24@gmail.com>
2026-02-18 16:47:54 -0500

    Deploy portfolio in github.

--- src/pages/Projects/ProjectCard.jsx
+++ src/pages/Projects/ProjectCard.jsx
@@ -1,15 +1,14 @@
 function ProjectCard(props) {
 
     return (
-        <div className="project-card-container" id="section-projects">
-                            <div className="project-card-title">{props.project.title}</div>
+        <div className="project-card-container" id="section-projects" onClick={() => window.open(props.project.demoUrl, "_blank")} >
 
             <div className="project-card-wrapper">
                 <img src={props.project.imageUrl}/>
             </div>
             <div className="project-card-content">
+            <div className="project-card-title">{props.project.title}</div>
                 <div className="project-card-btn-content">
-                    <button className="btn btn-primary" onClick={() => window.open(props.project.demoUrl, "_blank")}>Demo</button>
                     <button className="btn btn-primary" onClick={() => window.open(props.project.githubUrl, "_blank")}>Github</button>
                 </div>
                 <div>{props.project.technologies.map(technology =>  {return <span style={{marginLeft:6}}>{technology},</span>} )}</div>
